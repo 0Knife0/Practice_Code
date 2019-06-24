c60c4d437a0decfdc05f4bbbd03f4ceb596c4e90
zhuzilong <zhuzilong2013@gmail.com>
2019-06-24 15:04:34 +0800

    TopologySort

---
+++ C++代码/5_Code_03_TopologySort.cpp
@@ -0,0 +1,29 @@
+// class_05 TopologySort
+#include <iostream>
+#include <map>
+#include <queue>
+#include <vector>
+#include "6_GraphGenerator.h"
+
+using namespace std;
+
+static vector<Node*> sortedTopology(Graph grahp){
+    map<Node*,int> inMap;
+    queue<Node*> zeroInQueue;
+    for(auto node: grahp.nodes){
+        inMap.insert({node.second, node.second->in});
+        if(node.second->in == 0){
+            zeroInQueue.push(node.second);
+        }
+    }
+    vector<Node*> result;
+    while (!zeroInQueue.empty()){
+        Node* cur = zeroInQueue.front();
+        result.push_back(cur);
+        for(auto next: cur->nexts){
+            inMap.insert({next, inMap[next]-1});
+            if(inMap[next] == 0) zeroInQueue.push(next);
+        }
+    }
+    return result;
+}
